f9a2f42389d68d47d7587ed9d536592ef3f8a08f
lamisatahsin <158174513+lamisatahsin@users.noreply.github.com>
2024-03-04 09:18:04 +0600

    Add files via upload

---
+++ LAB-2[PHP]/largestnum.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 19;
+$b = 21;
+$c = 20;
+
+
+if($a > $b && $a > $c){
+    echo "a is the largest number";
+}else if($b > $a && $b > $c){
+    echo "b is the largest number";
+}else{
+    echo "c is the largest number";
+}
+
+?>
---
+++ LAB-2[PHP]/search_element.php
@@ -0,0 +1,17 @@
+<?php
+
+/*$Mates= array('Shafi', 'Sadiqur', 'Rahim' );
+
+echo $Mates[2]; */
+
+$Mates= [
+    'M1'=>['roll'=>1, 'name'=>'shafi', 'contact'=>'234567'],
+    'M2'=>['roll'=>2, 'name'=>'Sadiqur' ,'contact'=>'234578'],
+    'M3'=>['roll'=>3, 'name'=>'shafi', 'contact'=>'234552']
+];
+
+//echo $Mates['M2']['contact'];
+
+
+
+?>
